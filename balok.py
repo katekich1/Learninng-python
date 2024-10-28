6509825321a6548be7e58c769af1058bdcc93af9
= <=>
2024-10-29 06:47:43 +0700

    balok

--- balok.py
+++ balok.py
@@ -1,1 +1,11 @@
-print ("balok")
+print ("="*25)
+print ("\tbalok")
+print ("="*25)
+
+#rumus mencari volume
+p = int(input('masukan nilai panjang\t: '))
+l = int(input('masukan nilai panjang\t: '))
+t = int(input('masukan nilai panjang\t: '))
+
+v = p * l * t
+print ('volume balok adalah\t:' ,v, )

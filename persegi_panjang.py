c792634578184cbfa4e2098d55355f678c721afe
= <=>
2024-10-29 10:25:29 +0700

    persegi panjang

---
+++ persegi_panjang.py
@@ -0,0 +1,9 @@
+print ("="*25)
+print ("\tpersegi panjang")
+print ("="*25)
+
+p = int(input('masukan nilai panjang\t: '))
+l = int(input('masukan nilai lebar\t: '))
+
+L = p * l
+print ('hasil luas adalah\t: ' ,L)
---
+++ tabung.py
@@ -0,0 +1,4 @@
+print ("="*25)
+print ("\ttabung")
+print ("="*25)
+

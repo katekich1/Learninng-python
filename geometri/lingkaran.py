572adf87069e681c37ffe66dc68194cc72d060ac
= <=>
2024-11-15 07:47:13 +0700

    geometri

--- geometri/lingkaran.py
+++ geometri/lingkaran.py
@@ -1,5 +1,15 @@
-print ("="*25)
-print ("\tlingkaran")
-print ("="*25)
+def lingkaran():
+    print("=" *30)
+    print("\tRUMUS LINGKARAN")
+    print("=" *30)
 
-# mencari keliling lingkaran
+    PHI = 3.14
+    r = int(input("masukan nilai jari jari\t: "))
+
+    l = lambda r: PHI * r * r
+    k = lambda r: PHI * 2 * r 
+
+    print("luas lingkaran\t\t:" ,l(r))
+    print("keliling lingkaran\t\t:" ,k(r))
+
+lingkaran()
